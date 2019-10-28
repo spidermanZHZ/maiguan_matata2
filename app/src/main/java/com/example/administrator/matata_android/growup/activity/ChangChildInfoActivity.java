d24ec2d876c39c237c2a04fa27b562b2c6207415
123456 <1234567>
2019-10-28 22:01:54 +0800

    home 10/28 frist

--- app/src/main/java/com/example/administrator/matata_android/growup/activity/ChangChildInfoActivity.java
+++ app/src/main/java/com/example/administrator/matata_android/growup/activity/ChangChildInfoActivity.java
@@ -11,6 +11,8 @@ import android.widget.ImageView;
 
 import com.blankj.utilcode.util.ToastUtils;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.administrator.matata_android.R;
 import com.github.florent37.singledateandtimepicker.SingleDateAndTimePicker;
 import com.github.florent37.singledateandtimepicker.dialog.SingleDateAndTimePickerDialog;
@@ -41,14 +43,49 @@ public class ChangChildInfoActivity extends TakePhotoActivity {
     @BindView(R.id.adjust_btn_confim)
     Button adjustBtnConfim;
 
+//TakePhoto
+       private TakePhoto takePhoto;
+        private CropOptions cropOptions; //裁剪参数
+       private CompressConfig compressConfig;//压缩参数
+       private Uri imageUri; //图片保存路径
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chang_child_info);
         ButterKnife.bind(this);
-        ininData();
+        //ininData();
+        initDatas();
     }
+
+    private void initDatas(){
+     //获取TakePhoto实例
+    takePhoto = getTakePhoto();
+    //设置裁剪参数
+    cropOptions = new CropOptions.Builder().setAspectX(1).setAspectY(1).setWithOwnCrop(false).create();
+    //设置压缩参数
+    compressConfig=new CompressConfig.Builder().setMaxSize(50*1024).setMaxPixel(800).create();
+    takePhoto.onEnableCompress(compressConfig,true); //设置为需要压缩
+        changeChildHead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageUri=getImageCropUri();
+                //拍照并裁剪
+                //takePhoto.onPickFromCaptureWithCrop(imageUri,cropOptions);
+                //从相册中选取图片并裁剪
+                takePhoto.onPickFromGalleryWithCrop(imageUri, cropOptions);
+            }
+        });
+
+    }
+    //获得照片输出保存URL
+    private Uri getImageCropUri(){
+        File file=new File(Environment.getExternalStorageDirectory(),"/temp/"+System.currentTimeMillis()+".jpg");
+        if (!file.getParentFile().exists())file.getParentFile().mkdirs();
+        return Uri.fromFile(file);
+    }
+
 
     /**
      * 导入资源
@@ -86,9 +123,8 @@ public class ChangChildInfoActivity extends TakePhotoActivity {
     @Override
     public void takeSuccess(TResult result) {
         super.takeSuccess(result);
-        TImage image = result.getImage();
-
-        Glide.with(this).load(new File(image.getCompressPath())).into(changeChildHead);
+        String iconPath =result.getImage().getOriginalPath();
+        Glide.with(this).load(iconPath).apply(RequestOptions.bitmapTransform(new CircleCrop())).into(changeChildHead);
     }
 
     @Override
