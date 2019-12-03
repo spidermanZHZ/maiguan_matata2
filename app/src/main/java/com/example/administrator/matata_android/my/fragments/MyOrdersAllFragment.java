46dd24427c71d39e8a10e0fd2754ce610bd092e1
123456 <1234567>
2019-12-03 23:00:34 +0800

    12/3 21:40 commit-"我的订单页面"

--- app/src/main/java/com/example/administrator/matata_android/my/fragments/MyOrdersAllFragment.java
+++ app/src/main/java/com/example/administrator/matata_android/my/fragments/MyOrdersAllFragment.java
@@ -1,34 +1,74 @@
 package com.example.administrator.matata_android.my.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.administrator.matata_android.R;
 import com.example.administrator.matata_android.zhzbase.base.BaseFragment;
 
-public class MyOrdersAllFragment extends BaseFragment{
+public class MyOrdersAllFragment extends Fragment {
+    boolean mIsPrepare = false;		//视图还没准备好
+    boolean mIsVisible= false;		//不可见
+    boolean mIsFirstLoad = true;	//第一次加载
+    View rootView;
     @Override
-    protected int initLayoutId() {
-        return R.layout.fragment_all_orders;
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mIsPrepare=true;
+        lazyLoad();
     }
 
     @Override
-    protected void getExras() {
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        //isVisibleToUser这个boolean值表示:该Fragment的UI 用户是否可见
+        if (isVisibleToUser) {
+            mIsVisible = true;
+            lazyLoad();
+        } else {
+            mIsVisible = false;
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        if (rootView==null){
+            rootView=inflater.inflate(R.layout.fragment_all_orders,container,false);
+        }
+        return rootView;
+    }
+
+    /**
+     * 懒加载
+     */
+    private void lazyLoad(){
+
+    }
+
+    /**
+     * 加载数据
+     */
+    private void lazyData(){
 
     }
 
     @Override
-    protected void initData() {
-
+    public void onDestroyView() {
+        super.onDestroyView();
+        mIsFirstLoad=true;
+        mIsPrepare=false;
+        mIsVisible = false;
+        if (rootView!=null){
+            ((ViewGroup)rootView.getParent()).removeView(rootView);
+        }
     }
 
-    @Override
-    protected void setListener() {
-
-    }
-
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        return false;
-    }
 }
